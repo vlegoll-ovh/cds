81176c476829af94f2c4acabd74ba36575209099
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2020-01-13 15:12:50 +0100

    chore(engine): try another version of junit-converter, add time on rust
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- engine/api/application_deployment.go
+++ engine/api/application_deployment.go
@@ -23,9 +23,7 @@ func (api *API) getApplicationDeploymentStrategiesConfigHandler() service.Handle
 		app, err := application.LoadByName(api.mustDB(), api.Cache, key, appName, application.LoadOptions.WithDeploymentStrategies)
 		if err != nil {
 			return sdk.WrapError(err, "getApplicationDeploymentStrategiesConfigHandler")
-
 		}
-
 		return service.WriteJSON(w, app.DeploymentStrategies, http.StatusOK)
 	}
 }
